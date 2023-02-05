348671fce1251cb5ffb1157d3978d0ac1193aa32
Alexander Danielson <90790351+ardumn@users.noreply.github.com>
2023-02-05 13:27:48 -0600

    PASDA Comments and HTML Inventory

--- harvesting/08a-01_pasda/html2csv.py
+++ harvesting/08a-01_pasda/html2csv.py
@@ -23,38 +23,48 @@ Updated Nov 10, 2022
 by @karenmajewicz
 Adds landing page and full metadata link
 
+Updated Feb 5, 2023
+by ardumn
+Addtional comments
+
 '''
 
-import csv
-import time
-import urllib.request
-from bs4 import BeautifulSoup
+import csv # The csv module implements classes to read and write tabular data in CSV format.
+import time # This module provides various time-related functions.
+import urllib.request # The urllib.request module defines functions and classes which help in opening URLs (mostly HTTP)
+from bs4 import BeautifulSoup # For pulling data out of HTML and XML files
+
 # extract exising urls from local csv file
+
 urls = []
 
 ## read the rows from the output CSV from datasetURL.py
-with open(' .csv') as fr:
-    reader = csv.reader(fr)  # reader object
-    for row in reader:
-        urls.append(row)
+
+with open(' .csv') as fr: # Opens CSV file
+    reader = csv.reader(fr)  # Reader object assigned
+    for row in reader: # Scans each URL to be appended 
+        urls.append(row) # Appended to each row
 
 
 # store parsed elements for all urls
+
 parseElements = []
 
 for url in urls:
-	landingPage = str(url)
-	page = urllib.request.urlopen(url[0]).read()
-	soup = BeautifulSoup(page, "html.parser")
+	landingPage = str(url) # Creates the landing page based on URL extracted
+	page = urllib.request.urlopen(url[0]).read() # Basic webpage generated from URL parsed
+	soup = BeautifulSoup(page, "html.parser") # Pulling data from HTML Parser
 
-	titleField = soup.find(attrs={'id': 'Label1'})
-	dateField = soup.find(attrs={'id': 'Label2'})
-	publisherField = soup.find(attrs={'id': 'Label3'})
-	descriptionField = soup.find(attrs={'id': 'Label14'})
-	metadataLink = soup.find('a', href=True, text='Metadata')
-	downloadLink = soup.find('a', href=True, text='Download')
+	titleField = soup.find(attrs={'id': 'Label1'}) # Finds attributes in relation to title field and is labeled
+	dateField = soup.find(attrs={'id': 'Label2'})  # Finds attributes in relation to date field and is labeled
+	publisherField = soup.find(attrs={'id': 'Label3'}) # Finds attributes in relation to publisher field and is labeled
+	descriptionField = soup.find(attrs={'id': 'Label14'}) # Finds attributes in relation to description field and is labeled
+	metadataLink = soup.find('a', href=True, text='Metadata') # Finds attributes in relation to metadata field and is labeled
+	downloadLink = soup.find('a', href=True, text='Download') # Finds attributes in relation to download field and is labeled
 
-	title = titleField.text.strip()
+# Stripped fields if above parameters fail
+
+	title = titleField.text.strip() 
 	date = dateField.text.strip()
 	publisher = publisherField.text.strip()
 	description = descriptionField.text.strip()
@@ -64,8 +74,8 @@ for url in urls:
 	except:
 		download = 'nil'
 		
-	#default values
-	code = '08a-01'
+	#default values (Assigned default values for resource class)
+	code = '08a-01'  
 	accessRights = "Public"
 	accrualMethod = "HTML"
 	dateAccessioned = time.strftime('%Y-%m-%d')
